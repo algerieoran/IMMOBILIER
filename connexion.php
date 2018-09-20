ffed6c7ca40922cb2e8368612e948b0cda3c8398
alegerieoran <settibelkacem313@gmail.com>
2018-09-20 17:02:35 +0200

    c3
    connexion

--- connexion.php
+++ connexion.php
@@ -3,7 +3,9 @@
 $hote = 'localhost';
 $bdd = 'immobilier';
 $utilisateur = 'root';
-$passe = '';$pdoIMMO = new PDO('mysql:host='.$hote.';dbname='.$bdd,$utilisateur,$passe);
+$passe = '';
+
+$pdoIMMO = new PDO('mysql:host='.$hote.';dbname='.$bdd,$utilisateur,$passe);
 $pdoIMMO->exec("SET NAMES utf8");
 
 ?>
